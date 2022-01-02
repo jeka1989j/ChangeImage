cee4f36b25116d0401b494b9c26d286aa5067c16
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2022-01-02 12:12:54 +0200

    update MainActivity.kt file added  fun changeImageWeather() method

--- app/src/main/java/com/example/changeimage/MainActivity.kt
+++ app/src/main/java/com/example/changeimage/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.changeimage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.changeImgBtn)
+        val imageWeather = findViewById<ImageView>(R.id.imageView)
+
+        btn.setOnClickListener {
+            changeImageWeather(imageWeather)
+        }
+    }
+
+    fun changeImageWeather(imageWeather: ImageView) {
+        if (imageWeather.tag == "rain") {
+            imageWeather.setImageResource(R.drawable.sunny_weather)
+            imageWeather.tag = "sunny"
+        } else {
+            imageWeather.setImageResource(R.drawable.raining)
+            imageWeather.tag = "rain"
+        }
     }
 }
